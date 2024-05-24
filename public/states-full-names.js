e1aa2d400737a6b5f6d6c72975795a8ef8ba71de
john <36556216+shimspedy@users.noreply.github.com>
2024-05-24 16:09:21 +0000

    added meta date

--- public/states-full-names.js
+++ public/states-full-names.js
@@ -106,5 +106,13 @@
             }
         });
     }
-    
+    function updateMetaDescription(description) {
+        let metaDescription = document.querySelector('meta[name="description"]');
+        if (!metaDescription) {
+            metaDescription = document.createElement('meta');
+            metaDescription.name = "description";
+            document.head.appendChild(metaDescription);
+        }
+        metaDescription.content = description;
+    }
     document.addEventListener('DOMContentLoaded', replaceStateAbbreviations);
