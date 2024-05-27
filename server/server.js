54f6f43eba9d4451f6ee3add92a9e7c7ebef4000
john <36556216+shimspedy@users.noreply.github.com>
2024-05-26 19:11:55 -0700

    updated

--- server/server.js
+++ server/server.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Middleware to add canonical link tag
+function addCanonicalLinkTag(req, res, next) {
+    const originalSendFile = res.sendFile;
+    res.sendFile = function (filePath) {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                return res.status(500).send('Server Error');
+            }
+
+            const canonicalUrl = `https://www.fedpay.com${req.originalUrl}`;
+            const canonicalLinkTag = `<link rel="canonical" href="${canonicalUrl}">`;
+
+            const modifiedHtml = data.replace('</head>', `${canonicalLinkTag}</head>`);
+            res.send(modifiedHtml);
+        });
+    };
+    next();
+}
+
 app.use(express.static(path.join(__dirname, '../public')));
+
+// Apply the middleware
+app.use(addCanonicalLinkTag);
 
 app.get('/state/:state', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'state.html'));
@@ -12,7 +35,6 @@ app.get('/state/:state', (req, res) => {
 app.get('/:leostate', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'leostate.html'));
 });
-
 
 app.get('/leopay/:state', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'leopay.html'));
